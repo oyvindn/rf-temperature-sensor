240c4889c3fa2aac5d1b085c08f3b0bbada26186
Øyvind Nerbråten <oyvind.nerbraten@bekk.no>
2022-03-04 19:30:20 +0100

    Inital sketch code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# rf-temperature-sensor
-Battery driven wireless temperature sensor
+# Battery driven wireless temperature sensor
+
+Arduino based battery driven wireless temperature sensor.
+
+## Introduction
+
+## ATmega328 on a breadboard
+
+In order to save battery the ATmega328P is set to use its internal 8 MHz RC oscillator as a clock. This way we can set up a minimal circuit, eliminating the need for extra components like the the 16 MHz crystal. See the [From Arduino to a Microcontroller on a Breadboard](https://docs.arduino.cc/built-in-examples/arduino-isp/ArduinoToBreadboard) tutorial for instructions on how to burn a bootloader and setting up the minimal ATmega328 circuit on a breadboard.
+
+## Arduino sketch dependencies
+
+* [Arduino AVR Boards](https://github.com/arduino/ArduinoCore-avr)
+* [RadioHead Packet Radio library for embedded microprocessors](http://www.airspayce.com/mikem/arduino/RadioHead/)
+* [One Wire](https://github.com/PaulStoffregen/OneWire)
+* [Arduino Library for Maxim Temperature Integrated Circuits](https://github.com/milesburton/Arduino-Temperature-Control-Library)
+* [Low-Power](https://github.com/rocketscream/Low-Power)
+
+## Hardware
+
+* ATmega328P
+* HopeRF RFM69CW 13DBM 433Mhz RF Transceiver Module
+* DS18B20 Digital temperature sensor
+
+## Compile, upload and monitor ATmega328p using the Arduino CLI
+
+### Add secrets
+Copy `arduino_secrets.h.example` to `arduino_secrets.h` and change the example secret values.
+
+### Download and install Arduino IDE and/or CLI
+[Arduino CLI](https://arduino.github.io/arduino-cli/) is an all-in-one solution that provides Boards/Library Managers, sketch builder, board detection, uploader, and many other tools needed to use any Arduino compatible board and platform from command line or machine interfaces.
+
+## Download and burn bootloader
+Download the [ATmega328 on a breadboard bootloader](https://www.arduino.cc/en/uploads/Tutorial/breadboard-1-6-x.zip) and follow the instructions in the [From Arduino to a Microcontroller on a Breadboard](https://docs.arduino.cc/built-in-examples/arduino-isp/ArduinoToBreadboard) tutorial.
+
+### Compile sketch
+`arduino-cli compile -b breadboard:avr:atmega328bb -v rfm69-client`
+
+### Upload sketch to ATmega328P using e.g. a FTD-232R-3V3 USB to TTL Serial Cable:
+`arduino-cli upload -p /dev/cu.usbserial-FTC0DLEV -b breadboard:avr:atmega328bb -v rfm69-client`
+
+## Open the serial monitor
+`arduino-cli monitor -p /dev/cu.usbserial-FTC0DLEV -c baudrate=115200`
---
+++ rfm69-client.ino
@@ -0,0 +1,123 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <RHReliableDatagram.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LowPower.h>
+
+#include "arduino_secrets.h"
+
+#define DS18B20_TEMPERATURE_SENSOR_GPIO 9
+
+#define RF_INTERRUPT_GPIO 2
+#define RF_FREQUENCY 433.00
+#define RF_SERVER_ADDRESS 1
+#define RF_CLIENT_ADDRESS 2
+
+#define SLEEP_CYCLES 2
+
+RH_RF69 rf69Driver(SS, RF_INTERRUPT_GPIO);
+RHReliableDatagram radioManager(rf69Driver, RF_CLIENT_ADDRESS);
+
+OneWire oneWire(DS18B20_TEMPERATURE_SENSOR_GPIO);
+DallasTemperature temperatureSensors(&oneWire);
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+
+  initRadio();
+  temperatureSensors.begin();
+}
+
+void loop()
+{
+  float temperature = readTemperature();
+  long vcc = readVcc();
+  transmit(temperature, vcc);
+  powerDown(SLEEP_CYCLES);
+}
+
+float readTemperature()
+{
+  temperatureSensors.requestTemperatures();
+  float temp = temperatureSensors.getTempCByIndex(0);
+  return temp;
+}
+
+long readVcc()
+{
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+
+  delay(2);            // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC))
+    ; // measuring
+
+  uint8_t low = ADCL;  // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high << 8) | low;
+
+  // Vbat = 1100mV * 1024 / ADC value
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result;              // Vcc in millivolts
+}
+
+void transmit(float temperature, long vcc)
+{
+  char tempStr[10];
+  dtostrf(temperature, 1, 2, tempStr);
+  char message[40];
+  sprintf(message, "%s;%d", tempStr, vcc);
+  Serial.print("Sending message: ");
+  Serial.println(message);
+  if (!radioManager.sendtoWait((uint8_t *)message, sizeof(message), RF_SERVER_ADDRESS))
+  {
+    Serial.println("Transmission failed");
+  }
+}
+
+void powerDown(int times)
+{
+  rf69Driver.sleep();
+
+  for (int i = 0; i < times; i++)
+  {
+    Serial.print("Powering down in 8s ");
+    Serial.print(i + 1);
+    Serial.print("/");
+    Serial.println(times);
+    delay(5);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+}
+
+void initRadio()
+{
+  if (radioManager.init())
+  {
+    Serial.println("RFM69 - Init success");
+  }
+  else
+  {
+    Serial.println("RFM69 - Init failed");
+  }
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  if (rf69Driver.setFrequency(RF_FREQUENCY))
+  {
+    Serial.println("RFM69 - Frequency set to 433MHz");
+  }
+  else
+  {
+    Serial.println("RFM69 - Failed to set frequency");
+  }
+
+  uint8_t encryptionKey[] = SECRET_RF_ENCRYPTION_KEY;
+  rf69Driver.setEncryptionKey(encryptionKey);
+}
